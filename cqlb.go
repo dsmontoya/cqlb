40e8de68a82cb41d24c6f8a3694f36e9ad3bc5ad
gerardoentropy <gerardo@entropy.tech>
2018-01-09 12:07:20 -0600

    Change for Salomon service

--- cqlb.go
+++ cqlb.go
@@ -3,6 +3,7 @@ package cqlb
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
 
@@ -14,11 +15,14 @@ import (
 const (
 	insertQueryTemplate = "insert into %s (%s) values(%s);"
 	whereQueryTemplate  = "select %s from %s %s %s %s;"
+	updateQueryTemplate = "update %s set %s where %s;"
+	batchQueryTemplate  = "begin batch %s apply batch;"
 )
 
 type fieldTag struct {
-	Name      string
-	OmitEmpty bool
+	Name       string
+	OmitEmpty  bool
+	PrimaryKey bool
 }
 
 type Session struct {
@@ -83,8 +87,35 @@ func (s *Session) Find(slice interface{}) error {
 func (s *Session) Insert(v interface{}) error {
 	f := fields(v)
 	stmt := insertQuery(f)
-	fmt.Println("fields", f)
+	//fmt.Println("fields", f)
 	return s.s.Query(stmt, f["values"].([]interface{})...).Exec()
+}
+
+func (s *Session) Update(v interface{}) error {
+	f := fieldsUpdate(v)
+	stmt := updateQuery(f)
+	// fmt.Println("fields: ", f)
+	// fmt.Println("stmt: ", stmt)
+	return s.s.Query(stmt).Exec()
+}
+
+func (s *Session) Batch(stmts string) error {
+	stmt := batchQuery(stmts)
+	// fmt.Println("fields: ", f)
+	// fmt.Println("stmt: ", stmt)
+	return s.s.Query(stmt).Exec()
+}
+
+func (s *Session) GetUpdateStmt(v interface{}) string {
+	f := fieldsUpdate(v)
+	stmt := updateQuery(f)
+	return stmt
+}
+
+func (s *Session) GetInsertStmt(v interface{}) string {
+	f := fieldsInsert(v)
+	stmt := insertQuery(f)
+	return stmt
 }
 
 func (s *Session) Iter(value interface{}) *gocql.Iter {
@@ -113,6 +144,7 @@ func (s *Session) Iter(value interface{}) *gocql.Iter {
 	if prefetch := c.prefetch; prefetch > 0 {
 		q.Prefetch(prefetch)
 	}
+	//fmt.Println("num: ", q.Iter().NumRows())
 	return q.Iter()
 }
 
@@ -300,13 +332,177 @@ func fields(v interface{}) map[string]interface{} {
 	return result
 }
 
+func fieldsInsert(v interface{}) map[string]interface{} {
+	var names string
+	var slots string
+	var values []interface{}
+	strategies := make(map[string]interface{})
+	result := make(map[string]interface{})
+	value := reflect.ValueOf(v)
+	indirect := reflect.Indirect(value)
+	t := indirect.Type()
+	result["table_name"] = inflection.Plural(strings.ToLower(t.Name()))
+	for i := 0; i < t.NumField(); i++ {
+		var inf interface{}
+		var tagName string
+		f := t.Field(i)
+		fv := indirect.Field(i)
+		tag := tag(f)
+		fvIndirect := reflect.Indirect(fv)
+		if fvIndirect.IsValid() == false {
+			continue
+		}
+		inf = fvIndirect.Interface()
+		isZero := isZero(inf)
+		if isZero == true && tag.OmitEmpty == true {
+			continue
+		}
+		if i != 0 {
+			names += ","
+			slots += ","
+		}
+		if tag.Name != "" {
+			tagName = tag.Name
+		} else {
+			tagName = strings.ToLower(f.Name)
+		}
+		names += tagName
+		if fvIndirect.Kind() == reflect.Map {
+			slots += formatMap(fvIndirect)
+		} else {
+			slots += getValue(fvIndirect)
+		}
+		strategies[tagName] = inf
+		values = append(values, inf)
+	}
+	result["names"] = names
+	result["values"] = values
+	result["slots"] = slots
+	return result
+}
+
+func fieldsUpdate(v interface{}) map[string]interface{} {
+	var columnsUpdate string
+	var keysUpdate string
+	var values []interface{}
+	result := make(map[string]interface{})
+	value := reflect.ValueOf(v)
+	indirect := reflect.Indirect(value)
+	t := indirect.Type()
+	result["table_name"] = inflection.Plural(strings.ToLower(t.Name()))
+	for i := 0; i < t.NumField(); i++ {
+		var inf interface{}
+		var tagName string
+		f := t.Field(i)
+		fv := indirect.Field(i)
+		tag := tag(f)
+		fvIndirect := reflect.Indirect(fv)
+		if fvIndirect.IsValid() == false {
+			continue
+		}
+
+		inf = fvIndirect.Interface()
+		isZero := isZero(inf)
+		if isZero == true && tag.OmitEmpty == true {
+			continue
+		}
+		if tag.Name != "" {
+			tagName = tag.Name
+		} else {
+			tagName = strings.ToLower(f.Name)
+		}
+		if tag.PrimaryKey {
+			if keysUpdate != "" {
+				keysUpdate += " and "
+			}
+			//keysUpdate += tagName + " = ?"
+			keysUpdate += tagName + " = " + getValue(fvIndirect)
+		} else {
+			if columnsUpdate != "" {
+				columnsUpdate += ", "
+			}
+			if fvIndirect.Kind() == reflect.Map {
+				columnsUpdate += listMap(tagName, fvIndirect)
+			} else {
+				//columnsUpdate += tagName + " = ?"
+				columnsUpdate += tagName + " = " + getValue(fvIndirect)
+			}
+		}
+		values = append(values, inf)
+	}
+	result["columns"] = columnsUpdate
+	result["keys"] = keysUpdate
+	result["values"] = values
+
+	return result
+}
+
 func insertQuery(f map[string]interface{}) string {
 	query := fmt.Sprintf(insertQueryTemplate, f["table_name"], f["names"], f["slots"])
 	return query
 }
 
+func updateQuery(f map[string]interface{}) string {
+	query := fmt.Sprintf(updateQueryTemplate, f["table_name"], f["columns"], f["keys"])
+	return query
+}
+
+func batchQuery(s string) string {
+	query := fmt.Sprintf(batchQueryTemplate, s)
+	return query
+}
+
 func isZero(x interface{}) bool {
 	return reflect.DeepEqual(x, reflect.Zero(reflect.TypeOf(x)).Interface())
+}
+
+func listMap(nameMap string, fv reflect.Value) string {
+	keys := fv.MapKeys()
+	var columnsMap string
+	for i := 0; i < len(keys); i++ {
+		if columnsMap != "" {
+			columnsMap += ", "
+		}
+		columnsMap += nameMap + "[" + getValue(keys[i]) + "] = " + getValue(fv.MapIndex(keys[i]))
+	}
+
+	return columnsMap
+}
+
+func formatMap(fv reflect.Value) string {
+	keys := fv.MapKeys()
+	var columnsMap string
+	for i := 0; i < len(keys); i++ {
+		if columnsMap != "" {
+			columnsMap += ", "
+		}
+		columnsMap += getValue(keys[i]) + ": " + getValue(fv.MapIndex(keys[i]))
+	}
+
+	columnsMap = "{" + columnsMap + "}"
+
+	return columnsMap
+}
+
+func getValue(fv reflect.Value) string {
+	var value string
+	//fmt.Println(fv.Type().String())
+	switch fv.Type().String() {
+	case "string":
+		value = "'" + fv.String() + "'"
+	case "gocql.UUID":
+		value = fv.Interface().(gocql.UUID).String()
+	case "int", "int8", "int16", "int32", "int64":
+		value = strconv.FormatInt(fv.Int(), 10)
+	case "float32", "float64":
+		value = strconv.FormatFloat(fv.Float(), 'f', -1, 64)
+	case "bool":
+		value = strconv.FormatBool(fv.Bool())
+	default:
+		fmt.Println("Invalid type")
+		value = "error"
+	}
+	return value
 }
 
 func tag(f reflect.StructField) *fieldTag {
@@ -316,6 +512,9 @@ func tag(f reflect.StructField) *fieldTag {
 	ft.Name = opts[0]
 	if len(opts) > 1 && opts[1] == "omitempty" {
 		ft.OmitEmpty = true
+	}
+	if len(opts) > 1 && opts[1] == "primary_key" {
+		ft.PrimaryKey = true
 	}
 	return ft
 }
